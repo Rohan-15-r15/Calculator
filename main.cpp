44d0d834a81f406988cc41d765988e4b883f08c7
Rohan-15-r15 <67414377+Rohan-15-r15@users.noreply.github.com>
2021-02-26 21:27:21 +0530

    Initial Commit
    Simple Calculator App

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   string input;
+   int len;
+   cout<<"Enter input:";
+   cin>>input;
+   cout<<input<<endl;
+   len=input.length();
+   cout<<len;
+
+   char op[]={'*','/','+','-'};
+   char num={};
+   for(int j=0;j>len;j++)
+   {
+       op[i]=input[i];
+   }
+
+   for(int i=0;i>len;i++)
+   {
+       if(op[i]=='*' || op[i]=='/')
+       {
+
+
+       }
+   }
+
+
+
+
+       return 0;
+}
